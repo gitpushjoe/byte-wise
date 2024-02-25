d35cb37e4017bbe7769ec1ef1d68ba42ee562055
gitpushjoe <gitpushjoe@github.com>
2024-02-24 19:35:58 -0500

    Continue work on zeno

--- zeno/zeno.cpp
+++ zeno/zeno.cpp
@@ -2,8 +2,18 @@
 
 #include "zeno.h"
 
-namespace ZENO {
-	std::string INT(ZenoType value) {
-		return std::to_string(std::get<int>(value));
+namespace zeno {
+
+std::string INT(ZenoType value) {
+	return std::to_string(std::get<int>(value));
+}
+
+Zeno::Zeno() { }
+
+void Zeno::enroll(std::string name, int* value, std::unordered_set<ZenoAttr> attrs) {
+	if (!objects.count(name)) {
+		objects[name] = std::make_tuple(std::nullopt, attrs);
 	}
+}
+
 }
--- zeno/zeno.h
+++ zeno/zeno.h
@@ -8,14 +8,19 @@
 #include <variant>
 #include <memory>
 #include <optional>
+#include <unordered_map>
+#include <unordered_set>
 
-typedef std::variant<int, double, std::string, bool, std::vector<int>> ZenoType;
+namespace zeno {
+
+typedef std::variant<int, bool, std::vector<int>> ZenoType;
 typedef std::optional<ZenoType> ZenoParent;
 typedef std::function<std::string(ZenoType, std::optional<ZenoType>)> ZenoLogger;
 
-namespace ZENO {
-	std::string INT(ZenoType value);
-}
+std::string INT(ZenoType value);
+
+enum class ZenoAttr {
+};
 
 struct ZenoObject {
 	std::unique_ptr<ZenoType> value;
@@ -26,18 +31,21 @@ struct ZenoObject {
 
 class Zeno {
 public:
+	
+	Zeno();
+
 	void setCurrentLine(int line);
 	void setCurrentLines(int lines);
 
-	void enroll(std::string name, int* value);
-	void enroll(std::string name, double* value);
-	void enroll(std::string name, std::string* value);
-	void enroll(std::string name, bool* value);
+	void enroll(std::string name, int* value, std::unordered_set<ZenoAttr> attrs = {});
 	void enroll(std::string name, std::vector<int>* value);
 
 private:
 	std::string instructions;
+	std::unordered_map<std::string, std::tuple<ZenoParent, std::unordered_set<ZenoAttr>>> objects;
 };
+
+}
 
 #include "zeno.cpp"
 
