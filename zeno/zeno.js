a499690d93528c18df3446a2d12dc438d821a5d5
gitpushjoe <gitpushjoe@github.com>
2024-02-26 13:31:21 -0500

    Organize Zeno and begin implementation of zeno functions

--- zeno/zeno.js
+++ zeno/zeno.js
@@ -22,12 +22,24 @@ class Zeno {
 		return "[[SCOPE_NAME]]";
 	}
 
+	static get CALLER() {
+		return "[[CALLER]]";
+	}
+
 	static get BLOCK() {
 		return "BLOCK";
 	}
 
 	static get MAIN() {
 		return "MAIN";
+	}
+
+	static get FUNCTION() {
+		return "FUNCTION";
+	}
+
+	static get RETURN_VALUE() {
+		return "%RET";
 	}
 
 	static get FOR() {
@@ -54,8 +66,14 @@ class Zeno {
 		const func = (function () { return; }).bind(this);
 		const handler = {
 			apply: (_, __, args) => {
-				this.set(args[0], args[1]);
-				return true;
+				if (args.length === 0) {
+					const ret = this.find(Zeno.RETURN_VALUE);
+					while (this.currentScope().get(Zeno.SCOPE_TYPE) !== Zeno.FUNCTION) {
+						this.stack.pop();
+					}
+					return ret;
+				}
+				return this.function(...args);
 			},
 			get: (_, name) => {
 				return this.find(name);
@@ -67,7 +85,7 @@ class Zeno {
 			},
 		};
 		this.proxy = new Proxy(func, handler);
-		this.pushScope(Zeno.MAIN, "main", 0);
+		this.pushScope(Zeno.MAIN, "main", 0, "main()");
 	}
 
 	currentScope() {
@@ -94,8 +112,8 @@ class Zeno {
 		} while (this.scopeIdxIs(scopeIdx--, Zeno.BLOCK));
 	}
 
-	pushScope(type, name, section) {
-		this.stack.push(new Map([[Zeno.SCOPE_TYPE, type], [Zeno.SCOPE_NAME, name], [Zeno.SOURCE_SECTION, section]]));
+	pushScope(type, name, section, caller) {
+		this.stack.push(new Map([[Zeno.SCOPE_TYPE, type], [Zeno.SCOPE_NAME, name], [Zeno.SOURCE_SECTION, section], [Zeno.CALLER, caller]]));
 	}
 
 	set(name, value) {
@@ -126,10 +144,14 @@ class Zeno {
 		}
 		let value = init;
 		while (1) {
-			this.pushScope(Zeno.BLOCK, Zeno.FOR, section);
+			this.pushScope(Zeno.BLOCK, Zeno.FOR, section, this.currentScope().get(Zeno.CALLER) + "*");
 			this.set(name, value);
 			this.zap(section);
-			body(value);
+			const res = body(value);
+			if (res !== undefined) {
+				this.set(Zeno.RETURN_VALUE, res);
+				return res;
+			}
 			value = update(this.find(name));
 			this.set(name, value);
 			this.stack.pop();
@@ -142,14 +164,41 @@ class Zeno {
 	if(condition, section, body, elseBody) {
 		const success = condition();
 		if (success) {
-			this.pushScope(Zeno.BLOCK, Zeno.IF, section);
-			body();
+			this.pushScope(Zeno.BLOCK, Zeno.IF, section, this.currentScope().get(Zeno.CALLER) + "*");
+			const res = body();
+			if (res !== undefined) {
+				this.set(Zeno.RETURN_VALUE, res);
+				return res;
+			}
 			this.stack.pop();
 		} else if (elseBody !== undefined) {
-			this.pushScope(Zeno.BLOCK, Zeno.ELSE, section);
-			elseBody();
+			this.pushScope(Zeno.BLOCK, Zeno.ELSE, section, this.currentScope().get(Zeno.CALLER) + "*");
+			const res = elseBody();
+			if (res !== undefined) {
+				this.set(Zeno.RETURN_VALUE, res);
+				return res;
+			}
 			this.stack.pop();
 		}
+	}
+
+	function(name, section, argsNames, body) { 
+		return ((...args) => {
+			const caller = `${name}(${args.join(", ")})`;
+			this.pushScope(Zeno.FUNCTION, name, section, caller);
+			if (args.length !== argsNames.length) {
+				throw new Error(`Function "${name}" called with ${args.length} arguments, expected ${argsNames.length}`);
+			}
+			for (const i in argsNames) {
+				this.currentScope().set(argsNames[i], args[i]);
+			}
+			this.zap(section);
+			const [ resSection, result ] = body(...args);
+			this.set(Zeno.RETURN_VALUE, result);
+			this.zap(resSection);
+			this.stack.pop();
+			return result;
+		}).bind(this);
 	}
 
 	print() {
@@ -160,7 +209,6 @@ class Zeno {
 			for (let i = 0; i < data.length; i++) {
 				text += `Scope level ${i}: `;
 				for (const [key, value] of data[i]) {
-					// text += `(\x1b[33m"${key}"\x1b[0m: \x1b[36m${JSON.stringify(value)}\x1b[0m) `;
 					text += `\n\t\u001b[33m"${key}"\u001b[0m: \u001b[36m${JSON.stringify(value)}\u001b[0m`;
 				}
 				text += "\n";
@@ -168,56 +216,30 @@ class Zeno {
 			console.log(text);
 		});
 	}
+
+	printConcise() {
+		this.zaps.forEach((zap, idx) => {
+			const data = zap.snapshotData;
+			console.log(`Zap #${idx + 1} (Section ${zap.section})`);
+			for (let i = 0; i < data.length; i++) {
+				console.log(`Scope level ${i}: `);
+				process.stdout.write("\t");
+				let j = 0;
+				for (const [key, value] of data[i]) {
+					if (key.startsWith("[[")) {
+						process.stdout.write(`\u001b[36m${value} \u001b[0m `);
+						if (key === Zeno.CALLER) {
+							process.stdout.write("\n\t");
+						}
+						continue;
+					}
+					process.stdout.write(`"${key}": ${JSON.stringify(value)} `);
+				}
+				console.log();
+			}
+			console.log();
+		});
+	}
 };
 
-const zeno = new Zeno();
-const $ = zeno.proxy;
-const zap = zeno.zap.bind(zeno);
-
-$.people = [
-	["John", 15],
-	["Jane", 13],
-	["Mike", 20],
-	["Sara", 18],
-	["Tom", 25],
-	["Lily", 11],
-	["Bob", 30],
-	["Alice", 17],
-	["Eve", 31],
-	["Adam", 19]
-];
-
-zap(0);
-
-$.adults = [];
-$.children = [];
-
-zap(1);
-
-zeno.for("i", 0, (i) => i < $.people.length, (i) => ++i, 2, (i) => {
-	
-	$.person = $.people[i];
-	[ $.name, $.age ] = $.person;
-
-	zap(3);
-
-	zeno.if(() => $.age >= 18, 4, () => {
-		$.adults.push($.name);
-		zap(5);
-
-	}, () => {
-		$.children.push($.name);
-		zap(6);
-	});
-
-});
-
-$.adults.sort();
-$.children.sort();
-
-zap(7);
-
-zeno.print();
-
-console.log({ adults: $.adults, children: $.children });
-
+module.exports = Zeno;
